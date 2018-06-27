c8758d563d7b30690bd896a0f93bfc80e5cb8380
qwettst <36726540+qwettst@users.noreply.github.com>
2018-06-28 01:06:03 +0700

    0.0.01

--- app/src/main/java/com/example/zz/chilq/Auth.java
+++ app/src/main/java/com/example/zz/chilq/Auth.java
@@ -5,13 +5,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import co.ceryle.radiorealbutton.RadioRealButton;
+import co.ceryle.radiorealbutton.RadioRealButtonGroup;
+
+import static java.lang.Boolean.TRUE;
+
 public class Auth extends AppCompatActivity implements View.OnClickListener {
+
+    private RadioRealButton bParent, bChild;
+    private RadioRealButtonGroup radioRealButtonGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
         findViewById(R.id.signGoogle).setOnClickListener(this);
+        bParent = (RadioRealButton) findViewById(R.id.bParent);
+        bChild = (RadioRealButton) findViewById(R.id.bChild);
+        radioRealButtonGroup = (RadioRealButtonGroup) findViewById(R.id.bGroup);
+        radioRealButtonGroup.setPosition(0);
+
     }
 
     @Override
